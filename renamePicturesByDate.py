f21d5a84e7a4be7e2c78ff72201200750dd892d8
Greg Stoll <greg@gregstoll.com>
2017-06-09 19:44:11 -0500

    Initial commit

---
+++ renamePicturesByDate.py
@@ -0,0 +1,46 @@
+import sys, os, re, datetime
+sys.path.append(os.path.join(os.path.dirname(os.path.abspath(__file__)), 'exifpy'))
+import exifread
+
+def renamePhotos(path):
+	dirs = []
+	for name in os.listdir(path):
+		# only include years
+		if (re.match(r'^\d{4}$', name) and os.path.isdir(os.path.join(name, path))):
+			dirs.append(os.path.join(path, name))
+
+	#print(dirs)
+	for yearDir in dirs:
+		for root, dir, files in os.walk(yearDir):
+			for filename in files:
+				if not (filename.endswith('.JPG') or filename.endswith('.jpg')):
+					continue
+				if not (filename.startswith('IMG_')):
+					continue
+				pathname = os.path.join(root, filename)
+				#print(filename)
+				f = open(pathname, 'rb')
+				tags = exifread.process_file(f, details=False)
+				if 'EXIF DateTimeOriginal' not in tags:
+					print ("No DateTimeOriginal for %s!" % pathname)
+					continue
+				dtString = str(tags['EXIF DateTimeOriginal'])
+				#print('original ' + str(tags['EXIF DateTimeOriginal']))
+				dt = datetime.datetime.strptime(dtString, '%Y:%m:%d %H:%M:%S')
+				def makeFilename(localDt):
+					return localDt.strftime('CA_%Y%m%d_%H_%M_%S.jpg')
+				newFilename = makeFilename(dt)
+				#print('dig ' + str(tags['EXIF DateTimeDigitized']))
+				#for tag in tags.keys():
+				#	print(tag)
+
+				f.close()
+				while (os.path.isfile(os.path.join(root, newFilename))):
+					dt = dt + datetime.timedelta(seconds=1)
+					newFilename = makeFilename(dt)
+				print('%s -> %s' % (filename, newFilename))
+				os.rename(os.path.join(root, filename), os.path.join(root, newFilename))
+
+
+if (__name__ == '__main__'):
+	renamePhotos(r'C:\Users\Greg\Pictures')
